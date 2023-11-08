e4292890837a15087c32730d003ec57a82ec326f
hojin lee <121307215+hojinlee-98@users.noreply.github.com>
2023-11-08 17:51:26 +0900

    Add files via upload

--- transcripts_range_Nov072023_hj.sh
+++ transcripts_range_Nov072023_hj.sh
@@ -3,6 +3,6 @@
 gene=`echo $1`
 
 echo "start" > ./${gene}/gencode_v24lift37_${gene}_hj.txt
-grep "${gene}" 20230708_gencode_v24lift37_hj.txt | awk -F '\t' '{print $5}' | sort -n >> ./${gene}/gencode_v24lift37_${gene}_hj.txt
+grep "\s${gene}\s" 20230708_gencode_v24lift37_hj.txt | awk -F '\t' '{print $5}' | sort -n >> ./${gene}/gencode_v24lift37_${gene}_hj.txt
 echo "end" >> ./${gene}/gencode_v24lift37_${gene}_hj.txt
-grep "${gene}" 20230708_gencode_v24lift37_hj.txt | awk -F '\t' '{print $6}' | sort -n -r >> ./${gene}/gencode_v24lift37_${gene}_hj.txt
+grep "\s${gene}\s" 20230708_gencode_v24lift37_hj.txt | awk -F '\t' '{print $6}' | sort -n -r >> ./${gene}/gencode_v24lift37_${gene}_hj.txt
